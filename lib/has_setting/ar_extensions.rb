4bee339ae336cb5da85695c3b52393d7bc1cd1b6
Nico Ritsche <nico.ritsche@gmail.com>
2017-05-17 17:31:13 +0200

    Update ar_extensions.rb
    Fix has_setting_options missing in intermediate parent class

--- lib/has_setting/ar_extensions.rb
+++ lib/has_setting/ar_extensions.rb
@@ -88,9 +88,10 @@ module HasSetting
     klass = self.class
     option = klass.has_setting_options ? klass.has_setting_options[name] : nil
     while option.nil? and
-          klass.base_class != ActiveRecord::Base and
-          klass.base_class.respond_to?(:has_setting_options)
-      klass = klass.base_class
+          klass.superclass != ActiveRecord::Base and
+          klass.superclass.respond_to?(:has_setting_options) and
+          klass.superclass.has_setting_options
+      klass = klass.superclass
       option = klass.has_setting_options[name]
     end
     option
